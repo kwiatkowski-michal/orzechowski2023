840b605f7a4716e63dfd649209827fcf66a0d1fd
kwiatkowski-michal <m.kwiatkowski@zsebydgoszcz.onmicrosoft.com>
2023-09-11 19:22:58 +0200

    aha

--- src/Akcje.tsx
+++ src/Akcje.tsx
@@ -17,21 +17,6 @@ export default function Akcje() {
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum minus fugit tempora quae quaerat officia pariatur? Atque ipsam facere dolorem aspernatur accusamus eos cum maxime, praesentium earum laborum a? Tempore a quidem hic accusamus mollitia voluptas assumenda. Autem illo cupiditate sapiente facere reprehenderit maxime nihil assumenda quae vero adipisci unde deserunt eius laboriosam, corrupti perspiciatis, alias in beatae odit laudantium.
                         </Text>
                     </Stack>
-                </SimpleGrid>
-                <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10} mt={10}>
-                    <Stack spacing={4}>
-                        <Text fontSize={"2xl"} fontWeight={"bold"} color={useColorModeValue("brand.100", "gray.000")}>
-                            Okrągłe Stoły Wokół Zdrowia Psychicznego
-                        </Text>
-                        <Text color={useColorModeValue("black", "gray.300")} fontSize={'lg'}>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum minus fugit tempora quae quaerat officia pariatur? Atque ipsam facere dolorem aspernatur accusamus eos cum maxime, praesentium earum laborum a? Tempore a quidem hic accusamus mollitia voluptas assumenda. Autem illo cupiditate sapiente facere reprehenderit maxime nihil assumenda quae vero adipisci unde deserunt eius laboriosam, corrupti perspiciatis, alias in beatae odit laudantium.
-                        </Text>
-                    </Stack>
-                    <Flex boxShadow={"dark-lg"}>
-                        <Image alt={'Mateusz Orzechowski'} src={'/image1.jpeg'} objectFit={'cover'} />
-                    </Flex>
-                </SimpleGrid>
-                <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10} mt={10}>
                     <Flex boxShadow={"dark-lg"}>
                         <Image alt={'Mateusz Orzechowski'} src={'/image1.jpeg'} objectFit={'cover'} />
                     </Flex>
@@ -43,8 +28,9 @@ export default function Akcje() {
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum minus fugit tempora quae quaerat officia pariatur? Atque ipsam facere dolorem aspernatur accusamus eos cum maxime, praesentium earum laborum a? Tempore a quidem hic accusamus mollitia voluptas assumenda. Autem illo cupiditate sapiente facere reprehenderit maxime nihil assumenda quae vero adipisci unde deserunt eius laboriosam, corrupti perspiciatis, alias in beatae odit laudantium.
                         </Text>
                     </Stack>
-                </SimpleGrid>
-                <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10} mt={10}>
+                    <Flex boxShadow={"dark-lg"}>
+                        <Image alt={'Mateusz Orzechowski'} src={'/image1.jpeg'} objectFit={'cover'} />
+                    </Flex>
                     <Stack spacing={4}>
                         <Text fontSize={"2xl"} fontWeight={"bold"} color={useColorModeValue("brand.100", "gray.000")}>
                             Okrągłe Stoły Wokół Zdrowia Psychicznego
@@ -56,6 +42,25 @@ export default function Akcje() {
                     <Flex boxShadow={"dark-lg"}>
                         <Image alt={'Mateusz Orzechowski'} src={'/image1.jpeg'} objectFit={'cover'} />
                     </Flex>
+                    <Stack spacing={4}>
+                        <Text fontSize={"2xl"} fontWeight={"bold"} color={useColorModeValue("brand.100", "gray.000")}>
+                            Okrągłe Stoły Wokół Zdrowia Psychicznego
+                        </Text>
+                        <Text color={useColorModeValue("black", "gray.300")} fontSize={'lg'}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum minus fugit tempora quae quaerat officia pariatur? Atque ipsam facere dolorem aspernatur accusamus eos cum maxime, praesentium earum laborum a? Tempore a quidem hic accusamus mollitia voluptas assumenda. Autem illo cupiditate sapiente facere reprehenderit maxime nihil assumenda quae vero adipisci unde deserunt eius laboriosam, corrupti perspiciatis, alias in beatae odit laudantium.
+                        </Text>
+                    </Stack>
+                    <Flex boxShadow={"dark-lg"}>
+                        <Image alt={'Mateusz Orzechowski'} src={'/image1.jpeg'} objectFit={'cover'} />
+                    </Flex>
+                    <Stack spacing={4}>
+                        <Text fontSize={"2xl"} fontWeight={"bold"} color={useColorModeValue("brand.100", "gray.000")}>
+                            Okrągłe Stoły Wokół Zdrowia Psychicznego
+                        </Text>
+                        <Text color={useColorModeValue("black", "gray.300")} fontSize={'lg'}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum minus fugit tempora quae quaerat officia pariatur? Atque ipsam facere dolorem aspernatur accusamus eos cum maxime, praesentium earum laborum a? Tempore a quidem hic accusamus mollitia voluptas assumenda. Autem illo cupiditate sapiente facere reprehenderit maxime nihil assumenda quae vero adipisci unde deserunt eius laboriosam, corrupti perspiciatis, alias in beatae odit laudantium.
+                        </Text>
+                    </Stack>
                 </SimpleGrid>
             </Container>
         </Box>
